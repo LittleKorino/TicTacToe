d4b6a20e59d4333a88b7f9440c7b655c56a03fa5
LittleKorino <pervinkorinodasan@gmail.com>
2024-02-12 03:25:31 +0530

    Added color system and removed example

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+# Set colours
+darkmode = ["black","white"]
+whitemode = ["white","black"]
+boolDark = True
+
+if boolDark:
+    #Set colour pallette
+    color = darkmode
 
 while running:
     # poll for events
@@ -19,26 +25,12 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    screen.fill(color[0])
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    # limits FPS to 60 
+    clock.tick(60)
 
 pygame.quit()
