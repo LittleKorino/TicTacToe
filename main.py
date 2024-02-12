4a30e8caeb5dd504ef8d31b0b6d5513d5e4abf10
LittleKorino <pervinkorinodasan@gmail.com>
2024-02-13 03:25:39 +0530

    Added Gameplay features like drawing the board, and analysing the board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import drawXO
+import random
 
 # pygame setup
 pygame.init()
@@ -13,6 +14,12 @@ running = True
 (CentreX,CentreY) = (windowwidth/2, windowheight/2)
 Charwidth = 200
 
+#To render Text in the canvas
+text_font = pygame.font.SysFont('Arial', 50) 
+def drawText(text, font, colour, surface, x, y):
+    img = font.render(text, True, colour)
+    surface.blit(img, (x, y))
+
 # Set colours
 darkmode = ["black","white"]
 whitemode = ["white","black"]
@@ -24,6 +31,55 @@ if boolDark:
 else:
     color = whitemode
 
+
+def DrawBoard(board):
+      for i in range(3):
+        for j in range (3):
+            if board[i][j] == 0:
+                drawXO.drawX(screen, CentreX - Charwidth + j*Charwidth, CentreY - Charwidth + i*Charwidth, 0.75*Charwidth,color[1])
+            elif board[i][j] == 1:
+                drawXO.drawO(screen, CentreX - Charwidth + j*Charwidth, CentreY - Charwidth + i*Charwidth, 0.75*Charwidth,color[1])
+            else:
+                continue
+
+
+#Board Setup
+board = [["","",""],
+        ["","",""],
+        ["","",""]]
+
+Avialable_Space =   [[1,1,1],
+                    [1,1,1],
+                    [1,1,1]]
+
+player = [0,1]
+currentPlayer = player[0]
+gameOver = False
+
+# Check gameStatus
+def CheckGameStatus(board):
+    #Checking Horizontal
+    if board[0][0] == board[0][1] and board[0][1] == board[0][2]:
+        return board[0][0]
+    elif board[1][0] == board[1][1] and board[1][1] == board[1][2]:
+        return board[1][0]
+    elif board[2][0] == board[2][1] and board[2][1] == board[2][2]:
+        return board[2][0]  
+    #Checking Vertical
+    elif board[0][0] == board[1][0] and board[1][0] == board[2][0]:
+        return board[0][0]
+    elif board[0][1] == board[1][1] and board[1][1] == board[2][1]:
+        return board[0][1] 
+    elif board[0][2] == board[1][2] and board[1][2] == board[2][2]:
+        return board[0][2]
+    #Checking Diagonal
+    elif board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return board[0][0]
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        return board[0][2]
+    else:
+        return ""
+    
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -43,15 +99,38 @@ while running:
     pygame.draw.line(screen, color[1], (CentreX - 1.5*Charwidth , CentreY - 0.5*Charwidth), ((CentreX - 1.5*Charwidth) + 3*Charwidth , (CentreY - 0.5*Charwidth) ), 10)
     pygame.draw.line(screen, color[1], (CentreX - 1.5*Charwidth , CentreY + 0.5*Charwidth), ((CentreX - 1.5*Charwidth) + 3*Charwidth , (CentreY + 0.5*Charwidth) ), 10)
 
-    #Draw X
-    drawXO.drawX(screen, CentreX , CentreY , 0.75 * Charwidth,color[1])
-    #draw O
-    drawXO.drawO(screen,CentreX , CentreY - Charwidth, 0.75* Charwidth,color[1])
+   
+    #Draw the board
+
+    i = random.randint(0,2)
+    j = random.randint(0,2)
+    if currentPlayer == 0 and Avialable_Space[i][j]!= 0 and gameOver == False:
+        board[i][j] = currentPlayer
+        Avialable_Space[i][j] = 0
+        currentPlayer = player[1]
+        DrawBoard(board)
+        if CheckGameStatus(board) != "":
+            print("Player X wins!")
+            drawText("Player X wins", text_font, color[1], screen, 10, 10)
+            break
+    elif currentPlayer == 1 and Avialable_Space[i][j]!= 0 and gameOver == False:
+        board[i][j] = currentPlayer
+        Avialable_Space[i][j] = 0
+        currentPlayer = player[0]
+        DrawBoard(board)
+        if CheckGameStatus(board) != "":
+            print("Player O wins!")
+            drawText("Player O wins", text_font, color[1], screen, 10, 10)
+            break
+
+    else:
+        continue
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     # limits FPS to 60 
-    clock.tick(60)
+    clock.tick(1)
 
 pygame.quit()
 
